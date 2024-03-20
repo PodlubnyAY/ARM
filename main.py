ae0c8796489c113e7dfdbb54b00a358a72e3191a
PodlubnyAY <andrewpdlbn@gmail.com>
2024-03-20 11:38:54 +0300

    embeding custom parser to project

--- main.py
+++ main.py
@@ -1,97 +1,195 @@
-import argparse
+import re
 import pandas as pd
+from typing import List
+
 import tools
+from argparser import ArgParser
+
+parser = ArgParser()
 
 
-def set_args(subparser, name):
-    subparser.add_argument('input', type=str, help="Файл с данными для поиска зависимостей")
-    subparser.add_argument(
-        '-m', '--method', type=str, default='fpgrowth', choices=tools.METHODS,
-        help="Выбор метода поиска зависимостей (fpgrowth по умолчанию)"
-    )
-    subparser.add_argument(
-        '-r', type=str, default=None, 
-        help="Предполагаемый корневой атрибут (только для метода tree)",
-    )
-    subparser.add_argument('-v', '--verbose', action='store_true', help="Рассчитать все метрики")
-    subparser.add_argument(
-        '--metric', type=str, default='confidence', choices=tools.METRICS,
-        help="Выбрать метрику (confidence по умолчанию)",
-    )
-    if name == 'parsel_and_conclusion':
-        subparser.add_argument('--min_support', type=float, default=0.1)
-        subparser.add_argument('--min_threshold', type=float, default=0.1)
-    else:
-        subparser.add_argument(
-            '--min_support', type=float, default=0.05,
-            help="Указать требуемую нижниюю границу значения поддержки (0.05 по умолчанию)",
-        )
-        subparser.add_argument(
-            '--min_threshold', type=float, default=0.7,
-            help="Указать требуемую нижниюю границу значения выбранной метрики (0.7 по умолчанию)",
-        )
-
-
-parser = argparse.ArgumentParser()
-subparsers = parser.add_subparsers(title="Subcommands", dest="command")
-
-parser_parsel = subparsers.add_parser("parsel", help='Найти все заключения по заданной посылке')
-set_args(parser_parsel, "parsel")
-parser_parsel.add_argument("parsel", type=str, help="Посылка")
-
-parser_conclusion = subparsers.add_parser("conclusion", help='Найти все посылки по заданноому заключению')
-set_args(parser_conclusion, "conclusion")
-parser_conclusion.add_argument("conclusion", type=str)
-
-parser_parsel_and_conclusion = subparsers.add_parser(
-    "parsel_and_conclusion", help='Оценить заданные посылку и заключение'
-)
-set_args(parser_parsel_and_conclusion, "parsel_and_conclusion")
-parser_parsel_and_conclusion.add_argument("parsel", type=str)
-parser_parsel_and_conclusion.add_argument("conclusion", type=str)
-
-parser_factors = subparsers.add_parser(
-    "factors", 
-    help='Найти все логические зависимости для заданных свойств',
-)
-set_args(parser_factors, "factors")
-parser_factors.add_argument("factors", type=str, nargs='+', help="Факторы для поиска логических зависимостей")
-
-parser_all = subparsers.add_parser("all", help='Найти все возможные логические зависимости')
-set_args(parser_all, "all")
-
-
-def main(args):
+def read_file(file):
     try:
-        df = pd.read_excel(args.input)
+        df = pd.read_excel(file)
+        return df
     except FileNotFoundError:
-        print(f"{args.input} не найден")
-        return
+        print(f"{file} не найден")
+        exit(1)
 
-    if args.command == "factors":
-        df = tools.filter_columns(df, args.factors)
-    
-    method = tools.METHODS.get(args.method, tools.fpgrowth)
+
+def get_rules(method, df, min_support, min_threshold, tree_root):
+    method = tools.METHODS.get(method, tools.fpgrowth)
     base_rules = method(
-        df, min_support=args.min_support,
-        min_threshold=args.min_threshold,
-        root=args.r,
+        df, min_support=min_support,
+        min_threshold=min_threshold,
+        root=tree_root,
     )
-    rules = tools.ROUTER[args.command](base_rules, args)
-    if not args.verbose:
-        columns_dropped = list(tools.METRICS - {args.metric})
-        rules.drop(columns=columns_dropped, errors='ignore', inplace=True)
+    return base_rules
 
+
+def print_rules(rules, metric, save_to=None):
     rules = rules.round(2)
     if rules.shape[0]:
-        rules = rules.sort_values(by=['support', args.metric], ascending=False)
+        rules = rules.sort_values(by=['support', metric], ascending=False)
         print(rules.to_string(index=False))
+        if save_to:
+            if save_to.endswith('csv'):
+                rules.to_csv(save_to)
+            elif save_to.endswith('xlsx'):
+                rules.to_excel(save_to)
+            else:
+                print(f"Неверный формат файла {save_to}. Необходим .csv или .xlsx")
     else:
         print("Нет логических зависимостей с заданными ограничениями")
 
 
-if __name__ == '__main__':
-    args = parser.parse_args(
-        'all --method apriori -r Feature2  --min_threshold 0.69 validate_input.xlsx'.split()
+@parser.add
+def print_features(input_file:str,):
+    """Вывести названия аттрибутов"""
+    data = read_file(input_file)
+    print(*data.columns)
+
+
+@parser.add
+def all_rules(
+    input_file:str, 
+    min_support=tools.MIN_SUPPORT, 
+    min_threshold=tools.MIN_THRESHOLD, 
+    method="fpgrowth",
+    verbose=False, 
+    metric="confidence", 
+    tree_root=None, 
+    save_to=None, 
+):
+    """Найти все возможные логические зависимости"""
+    data = read_file(input_file)
+    rules = get_rules(method, data, min_support, min_threshold, tree_root)
+    if not verbose:
+        columns_dropped = list(tools.METRICS - {metric})
+        rules.drop(columns=columns_dropped, errors='ignore', inplace=True)
+    print_rules(rules, metric, save_to=save_to)
+
+
+@parser.add
+def factor(
+    input_file:str, 
+    factors:List[str], 
+    min_support=tools.MIN_SUPPORT, 
+    min_threshold=tools.MIN_THRESHOLD,
+    method="fpgrowth", 
+    verbose=False, 
+    metric="confidence", 
+    tree_root=None, 
+    save_to=None,
+):
+    """Найти все логические зависимости для заданных свойств"""
+    data = read_file(input_file)
+    if isinstance(factors, str):
+        factors = factors.split()
+        
+    data = data[factors]
+    rules = get_rules(method, data, min_support, min_threshold, tree_root)
+    if not verbose:
+        columns_dropped = list(tools.METRICS - {metric})
+        rules.drop(columns=columns_dropped, errors='ignore', inplace=True)
+    print_rules(rules, metric, save_to=save_to)
+
+
+@parser.add
+def parsel(
+    input_file:str, 
+    parsel:str,
+    min_support=tools.MIN_SUPPORT, 
+    min_threshold=tools.MIN_THRESHOLD, 
+    method="fpgrowth", 
+    verbose=False, 
+    metric="confidence", 
+    tree_root=None, 
+    save_to=None,
+):
+    """Найти все заключения по заданной посылке"""
+    data = read_file(input_file)
+    rules = get_rules(method, data, min_support, min_threshold, tree_root)
+    rules = tools.filter_rows_postprocessing(data, parsel, "antecedents")
+    if not verbose:
+        columns_dropped = list(tools.METRICS - {metric})
+        rules.drop(columns=columns_dropped, errors='ignore', inplace=True)
+    print_rules(rules, metric, save_to=save_to)
+
+
+@parser.add
+def conclusion(
+    input_file:str, 
+    conclusion:str,
+    min_support=tools.MIN_SUPPORT, 
+    min_threshold=tools.MIN_THRESHOLD, 
+    method="fpgrowth", 
+    verbose=False, 
+    metric="confidence", 
+    tree_root=None, 
+    save_to=None,
+):
+    """Найти все посылки по заданноому заключению"""
+    data = read_file(input_file)
+    rules = get_rules(method, data, min_support, min_threshold, tree_root)
+    rules = tools.filter_rows_postprocessing(data, conclusion, "consequents")
+    if not verbose:
+        columns_dropped = list(tools.METRICS - {metric})
+        rules.drop(columns=columns_dropped, errors='ignore', inplace=True)
+    print_rules(rules, metric, save_to=save_to)
+
+
+@parser.add
+def parsel_conclusion(
+    input_file:str,
+    parsel:str,
+    conclusion:str,
+    verbose=False,
+    metric="confidence",
+    save_to=None,
+):
+    metric_dict = {
+    "antecedent support": lambda _, sA, __: sA,
+    "consequent support": lambda _, __, sC: sC,
+    "support": lambda _, sA, __: sA,
+    "confidence": lambda sAC, sA, _: sAC / sA,
+}
+    """Оценить заданные посылку и заключение"""
+    pattern = r"[\w\d_-]+\s*(={1,2})\s*(?:\d+|'[\w\d_-]+')([\s,]&\s?)?"
+    data = read_file(input_file)
+    if match := re.match(pattern, parsel):
+        if match.group(1) != "==":
+            parsel = parsel.replace(match.group(1), "==")
+    else:
+        print(f"Неверно введена посылка: {conclusion}")
+        return
+    if match := re.match(pattern, conclusion):
+        if match.group(1) != "==":
+            conclusion = conclusion.replace(match.group(1), "==")
+    else:
+        print(f"Неверно введено заключение: {conclusion}")
+        return
+
+    args = (
+        data.query(f"{parsel} & {conclusion}").shape[0] / data.shape[0], 
+        data.query(parsel).shape[0] / data.shape[0], 
+        data.query(conclusion).shape[0] / data.shape[0], 
     )
-    main(args)
+    base_dict = {
+        "antecedents": [parsel], 
+        "consequents": [conclusion], 
+    }
+    base_dict |= {name : [m(*args)] for name, m in metric_dict.items()}
+    rule = pd.DataFrame(base_dict)
+    if not verbose:
+        columns_dropped = list(tools.METRICS - {metric})
+        rule.drop(columns=columns_dropped, errors='ignore', inplace=True)
+    print_rules(rule, metric, save_to=save_to)
+
+
+def main():
+    parser.run()
+
+
+if __name__ == '__main__':
+    main()
